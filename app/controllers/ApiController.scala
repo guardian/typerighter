6a5d210d6d6b68af3726d245a061a6ff994be3ac
Simon Hildrew <simon@hildrew.net>
2018-10-30 14:36:42 +0000

    Add healthcheck endpoint

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -10,15 +10,12 @@ import services.LanguageTool
  */
 class ApiController (cc: ControllerComponents, lt: LanguageTool) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok("{}")
+  }
+
+  def healthcheck() = Action { implicit request: Request[AnyContent] =>
+    Ok("""{ "healthy" : "true" }""")
   }
 
   def check = Action(parse.formUrlEncoded) { request =>
