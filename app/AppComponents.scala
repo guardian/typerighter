b059ffeb521ae0770745b38166406d258c945a2c
Simon Hildrew <simon@hildrew.net>
2018-11-06 16:49:15 +0100

    Change logging and disable the allowed hosts filter

--- app/AppComponents.scala
+++ app/AppComponents.scala
@@ -1,16 +1,23 @@
 import controllers.ApiController
 import play.api.ApplicationLoader.Context
 import play.api.BuiltInComponentsFromContext
+import play.api.mvc.EssentialFilter
 import play.filters.HttpFiltersComponents
 import router.Routes
 import services.LanguageTool
+import utils.Loggable
 
 class AppComponents(context: Context)
   extends BuiltInComponentsFromContext(context)
-  with HttpFiltersComponents {
+  with HttpFiltersComponents
+  with Loggable {
+
+  override def httpFilters: Seq[EssentialFilter] = super.httpFilters.filterNot(allowedHostsFilter ==)
+
+  logger.info(s"Starting with ${httpFilters.size} filters")
 
   // TODO: Provide an instance
-  val languageTool = LanguageTool.createInstance(None)
+  val languageTool: LanguageTool = LanguageTool.createInstance(None)
 
   val apiController = new ApiController(controllerComponents, languageTool)
 
--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -3,7 +3,10 @@ import play.api.ApplicationLoader.Context
 import play.filters.HttpFiltersComponents
 
 class AppLoader extends ApplicationLoader {
-  def load(context: Context) = {
+  def load(context: Context): Application = {
+    LoggerConfigurator(context.environment.classLoader).foreach {
+      _.configure(context.environment, context.initialConfiguration, Map.empty)
+    }
     new AppComponents(context).application
   }
 }
