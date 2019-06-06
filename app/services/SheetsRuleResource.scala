68ced6b7746ee9bc00373a925f3c282a36c978ee
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2019-06-06 15:54:23 +0100

    Switch to service account for Google auth

--- app/services/SheetsRuleResource.scala
+++ app/services/SheetsRuleResource.scala
@@ -1,20 +1,17 @@
 package services
 
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.googleapis.auth.oauth2.{GoogleCredential}
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.store.MemoryDataStoreFactory
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.SheetsScopes
 import java.io._
 import java.util.Collections
+
 import scala.collection.JavaConverters._
 import model.{Category, PatternRule, PatternToken}
-import play.api.{Configuration}
+import play.api.Configuration
 
 import scala.util.{Failure, Success, Try}
 
@@ -22,6 +19,7 @@ object SheetsRuleResource {
   private val APPLICATION_NAME = "Google Sheets API Java Quickstart"
   private val JSON_FACTORY = JacksonFactory.getDefaultInstance
   private val TOKENS_DIRECTORY_PATH = "tokens"
+
   /**
     * Global instance of the scopes required by this quickstart.
     * If modifying these scopes, delete your previously saved tokens/ folder.
@@ -31,40 +29,31 @@ object SheetsRuleResource {
   /**
     * Creates an authorized Credential object.
     *
+    * @param credentialsFilePath A string containing the credentials file path.
+    *                            These can be generated in Google Platform at
+    *                            https://console.cloud.google.com/apis/credentials.
     * @param HTTP_TRANSPORT The network HTTP Transport.
     * @return An authorized Credential object.
     * @throws IOException If the credentials.json file cannot be found.
     */
-  private def getCredentials(configuration: Configuration, HTTP_TRANSPORT: NetHttpTransport) = {
-    // Load client secrets.
-    val clientSecrets = new GoogleClientSecrets()
-    val details = new GoogleClientSecrets.Details()
-    for {
-      clientId <- configuration.getOptional[String]("typerighter.google.api.client.id")
-      clientSecret <- configuration.getOptional[String]("typerighter.google.api.client.secret")
-      clientRedirectUri <- configuration.getOptional[String]("typerighter.google.api.client.redirectUri")
-    } yield {
-      details.setAuthUri("https://accounts.google.com/o/oauth2/auth")
-      details.setTokenUri("https://oauth2.googleapis.com/token")
-      details.setClientId(clientId)
-      details.setClientSecret(clientSecret)
-      details.setRedirectUris(List(clientRedirectUri).asJava)
-      clientSecrets.setWeb(details)
-
-      // Build flow and trigger user authorization request.
-      val flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES).setDataStoreFactory(new MemoryDataStoreFactory).setAccessType("offline").build
-      val receiver = new LocalServerReceiver.Builder().setPort(8000).build
-      new AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
-    }
+  private def getCredentials(credentialsFilePath: String, HTTP_TRANSPORT: NetHttpTransport) = {
+    val in = new FileInputStream(credentialsFilePath)
+    if (in == null) throw new FileNotFoundException("Resource not found: " + credentialsFilePath)
+    val credential = GoogleCredential
+      .fromStream(in)
+      .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS))
+    in.close
+    credential
   }
 
   def getDictionariesFromSheet(configuration: Configuration): (List[PatternRule], List[String]) = { // Build a new authorized API client service.
     val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport
     val maybeResult = for {
+      credentialsPath <- configuration.getOptional[String]("typerighter.google.credentials.filePath")
       spreadsheetId <- configuration.getOptional[String]("typerighter.sheetId")
       range <- configuration.getOptional[String]("typerighter.sheetRange")
-      credentials <- getCredentials(configuration, HTTP_TRANSPORT)
     } yield {
+      val credentials = getCredentials(credentialsPath, HTTP_TRANSPORT)
       val service = new Sheets.Builder(
         HTTP_TRANSPORT,
         JSON_FACTORY,
