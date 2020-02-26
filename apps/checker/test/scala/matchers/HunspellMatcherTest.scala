762b95baf252858a69da9784299e6fd70bf4eee3
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-02-26 17:13:24 +0000

    Add test dictionary

--- apps/checker/test/scala/matchers/HunspellMatcherTest.scala
+++ apps/checker/test/scala/matchers/HunspellMatcherTest.scala
@@ -19,26 +19,60 @@ class HunspellMatcherTest extends AsyncFlatSpec with Matchers {
     suggestions = suggestions.map(s => TextSuggestion(s))
   )
 
-  "check" should "report single matches" in {
+  "check" should "not match for known words" in {
     val eventuallyMatches = hunspellMatcher.check(
-      MatcherRequest(getBlocks("example text"), "example-category")
+      MatcherRequest(getBlocks("Example text with John Smith"), "example-category")
+    )
+    eventuallyMatches.map { matches =>
+      matches shouldEqual Nil
+    }
+  }
+
+  "check" should "fuzzy match known words with suggestions" in {
+    val eventuallyMatches = hunspellMatcher.check(
+      MatcherRequest(getBlocks("Example text with John Simth"), "example-category")
     )
     eventuallyMatches.map { matches =>
       matches shouldEqual List(
-        getMatch("text", 8, 12, List("example"))
+        getMatch("Simth", 23, 28, List("Smith")),
       )
     }
   }
 
   "check" should "report multiple matches" in {
     val eventuallyMatches = hunspellMatcher.check(
-      MatcherRequest(getBlocks("text text text"), "example-category")
+      MatcherRequest(getBlocks("Example unknown with John Simth"), "example-category")
     )
     eventuallyMatches.map { matches =>
       matches shouldEqual List(
-        getMatch("text", 0, 4, List("example")),
-        getMatch("text", 5, 9, List("example")),
-        getMatch("text", 10, 14, List("example"))
+        getMatch("unknown", 8, 15, Nil),
+        getMatch("Simth", 26, 31, List("Smith")),
+      )
+    }
+  }
+
+  "check" should "correctly handle pluralisation" in {
+    val eventuallyMatches = hunspellMatcher.check(
+      MatcherRequest(getBlocks("Example word with John Smiths"), "example-category")
+    )
+    eventuallyMatches.map { matches =>
+      matches shouldEqual List(
+        getMatch("Smiths", 23, 29, List(
+          // It looks like Hunspell's fuzzy matching is very fuzzy for small dictionaries.
+          "Smith",
+          "Smith's",
+          "With")),
+      )
+    }
+  }
+
+  "check" should "report unknown words" in {
+    val eventuallyMatches = hunspellMatcher.check(
+      MatcherRequest(getBlocks("Example text with unknown word"), "example-category")
+    )
+    eventuallyMatches.map { matches =>
+      matches shouldEqual List(
+        getMatch("unknown", 18, 25, Nil)
       )
     }
   }
