1274643a90ceca09ab886009ea07b12127accb10
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-07-13 12:37:48 +0100

    Update components to add Search Matches functionality

--- rule-audit-client/src/components/capiFeed/CapiResults.tsx
+++ rule-audit-client/src/components/capiFeed/CapiResults.tsx
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
-import AppTypes from "AppTypes";
+import React from "react";
+import classnames from "classnames";
 import { connect } from "react-redux";
 
+import AppTypes from "AppTypes";
 import { selectors as capiSelectors } from "redux/modules/capiContent";
-import { selectors as uiSelectors } from "redux/modules/ui";
-import { CapiContentWithMatches } from "services/capi";
-import { notEmpty } from "utils/predicates";
+import {
+  selectors as uiSelectors,
+  actions as uiActions,
+} from "redux/modules/ui";
+import { selectors as searchMatchesSelectors } from "redux/modules/searchMatches";
 import CapiFeedItem from "./CapiFeedItem";
+import { bindActionCreators } from "redux";
 
-type IProps = ReturnType<typeof mapStateToProps>;
+type IProps = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
 
 const articleNumberFormat = new Intl.NumberFormat("en-GB");
 const checkboxId = "capi-results__show-all-articles";
 
-const CapiResults = ({ articleIds, isLoading, pagination }: IProps) => {
+const CapiResults = ({
+  articleIds,
+  isLoading,
+  pagination,
+  searchMatchesLoadingText,
+  displayAllArticles,
+  doToggleDisplayAllArticles,
+}: IProps) => {
   const totalArticles = pagination
     ? articleNumberFormat.format(
         articleIds.length < pagination.pageSize
@@ -21,29 +33,69 @@ const CapiResults = ({ articleIds, isLoading, pagination }: IProps) => {
           : pagination.totalPages * pagination.pageSize
       )
     : "?";
+
   return (
     <>
-      {isLoading ? (
-        <h5 className="text-secondary mt-2 mb-0">Loading</h5>
-      ) : (
-        <h5 className="mt-2 mb-0">
-          {articleIds.length} of {totalArticles} articles
-        </h5>
-      )}
+      <p className={classnames("mt-2 mb-0", { "text-secondary": isLoading })}>
+        <span className="h5">{`Showing ${articleIds.length}  of ${totalArticles} articles`}</span>
+        {isLoading && (
+          <span
+            className="spinner-border spinner-border-sm text-primary float-right"
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
+          </span>
+        )}
+      </p>
+      {searchMatchesLoadingText && <p>{searchMatchesLoadingText}</p>}
+      <div className="form-check form-check-inline mt-2">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id={checkboxId}
+          checked={displayAllArticles}
+          onChange={doToggleDisplayAllArticles}
+        />
+        <label className="form-check-label" htmlFor={checkboxId}>
+          <small>Show articles that don't have matches</small>
+        </label>
+      </div>
       <div className="list-group mt-2">
         {articleIds.map((id) => (
-          <CapiFeedItem id={id} />
+          <CapiFeedItem id={id} key={id} />
         ))}
       </div>
     </>
   );
 };
 
-const mapStateToProps = (state: AppTypes.RootState) => ({
-  articleIds: capiSelectors.selectLastFetchedArticleIds(state),
-  isLoading: capiSelectors.selectIsLoading(state),
-  pagination: capiSelectors.selectPagination(state),
-  selectedArticle: uiSelectors.selectSelectedArticle(state),
-});
+const mapStateToProps = (state: AppTypes.RootState) => {
+  const selectAllArticles = uiSelectors.selectDisplayAllArticles(state);
+  const searchMode = uiSelectors.selectSearchMode(state);
 
-export default connect(mapStateToProps)(CapiResults);
+  return {
+    articleIds:
+      searchMode === "MATCHES"
+        ? searchMatchesSelectors.selectSearchMatchesArticleIds(state, selectAllArticles)
+        : capiSelectors.selectLastFetchedArticleIds(state, selectAllArticles),
+    isLoading:
+      capiSelectors.selectIsLoading(state) ||
+      searchMatchesSelectors.selectIsSearchMatchesInProgress(state),
+    searchMatchesLoadingText: searchMatchesSelectors.selectSearchMatchesLoadingText(
+      state
+    ),
+    pagination: capiSelectors.selectPagination(state),
+    selectedArticle: uiSelectors.selectSelectedArticle(state),
+    displayAllArticles: uiSelectors.selectDisplayAllArticles(state),
+  };
+};
+
+const mapDispatchToProps = (dispatch: AppTypes.Dispatch) =>
+  bindActionCreators(
+    {
+      doToggleDisplayAllArticles: uiActions.doToggleDisplayAllArticles,
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(CapiResults);
