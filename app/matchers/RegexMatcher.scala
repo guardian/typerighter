c333578b87a8f5589493ad5fc107ee44b876bd06
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-09-18 15:36:40 +0100

    Add execution context to .check method, and invoke check within Future to ensure it's used to run regex checks

--- app/matchers/RegexMatcher.scala
+++ app/matchers/RegexMatcher.scala
@@ -4,9 +4,8 @@ import model.{RegexRule, RuleMatch}
 import services.MatcherRequest
 import utils.{Matcher, RuleMatchHelpers}
 
-
 import scala.concurrent.Future
-
+import scala.concurrent.ExecutionContext
 
 /**
   * A Matcher for rules based on regular expressions.
@@ -14,12 +13,13 @@ import scala.concurrent.Future
 class RegexMatcher(category: String, rules: List[RegexRule]) extends Matcher {
   def getId() = "regex-validator"
 
-  override def check(request: MatcherRequest): Future[List[RuleMatch]] = {
-    val matches = rules.foldLeft(List.empty[RuleMatch])((acc, rule) => {
-      val matches = checkRule(request, rule)
-      RuleMatchHelpers.removeOverlappingRules(acc, matches) ++ matches
-    })
-    Future.successful(matches)
+  override def check(request: MatcherRequest)(implicit ec: ExecutionContext): Future[List[RuleMatch]] = {
+    Future {
+      rules.foldLeft(List.empty[RuleMatch])((acc, rule) => {
+        val matches = checkRule(request, rule)
+        RuleMatchHelpers.removeOverlappingRules(acc, matches) ++ matches
+      })
+    }
   }
 
   override def getRules(): List[RegexRule] = rules
@@ -28,12 +28,13 @@ class RegexMatcher(category: String, rules: List[RegexRule]) extends Matcher {
 
   private def checkRule(request: MatcherRequest, rule: RegexRule): List[RuleMatch] = {
     request.blocks.flatMap { block =>
-        rule.regex.findAllMatchIn(block.text).map { currentMatch =>
-          rule.toMatch(
-            currentMatch.start,
-            currentMatch.end,
-            block
-          ) }
+      rule.regex.findAllMatchIn(block.text).map { currentMatch =>
+        rule.toMatch(
+          currentMatch.start,
+          currentMatch.end,
+          block
+        )
+      }
     }
   }
 }
