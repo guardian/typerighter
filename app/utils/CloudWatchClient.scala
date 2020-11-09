e5f46545c0cb8c816fba051c60e461599d3048c4
Sam Hession <samhession@live.co.uk>
2020-11-09 10:40:23 +0000

    Adds to putMetricDta logging

--- app/utils/CloudWatchClient.scala
+++ app/utils/CloudWatchClient.scala
@@ -29,7 +29,7 @@ class CloudWatchClient(stage: String, dryRun: Boolean) extends Loggable {
 
     try {
       cloudWatchClient.map(_.putMetricData(request))
-      log.info(s"Published metric data")
+      log.info(s"Published $metric metric data. ${if (dryRun) "DRY RUN" else ""}")
     } catch {
       case e: Exception =>
         log.error(s"CloudWatch putMetricData exception message: ${e.getMessage}", e)
