c4ec1d10ba426f73a5b1b812245b37581857e466
Sam Hession <samhession@live.co.uk>
2020-11-19 14:25:18 +0000

    Adds basic app loader

--- rule-manager/app/controllers/HomeController.scala
+++ rule-manager/app/controllers/HomeController.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import javax.inject._
 import play.api._
 import play.api.mvc._
 
@@ -8,16 +7,9 @@ import play.api.mvc._
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-@Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+class HomeController(val controllerComponents: ControllerComponents) extends BaseController {
+
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
