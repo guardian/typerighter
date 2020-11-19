3cfb4ebe4f0829c35a53dc71b5b5b128609c8e3e
Sam Hession <samhession@live.co.uk>
2020-11-19 10:18:41 +0000

    Adds healthcheck endpoint

--- rule-manager/app/controllers/HomeController.scala
+++ rule-manager/app/controllers/HomeController.scala
@@ -21,4 +21,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
+  def healthcheck() = Action { implicit request: Request[AnyContent] =>
+    Ok("""{ "healthy" : "true" }""")
+  }
 }
