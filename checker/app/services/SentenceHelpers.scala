163eddc3271e5e4158188920fec74195aabbb200
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-11-24 10:00:16 +0000

    Avoid combinatorial when calculating sentence starts

--- checker/app/services/SentenceHelpers.scala
+++ checker/app/services/SentenceHelpers.scala
@@ -16,6 +16,7 @@ case class WordInSentence(sentence: String, word: String, range: TextRange)
   * A service to extract proper names from documents.
   */
 class SentenceHelpers() {
+  println("new sentence annotator")
   val props: Properties = new Properties()
   props.put("annotators", "tokenize, ssplit")
   val pipeline: StanfordCoreNLP = new StanfordCoreNLP(props)
@@ -25,18 +26,17 @@ class SentenceHelpers() {
     pipeline.annotate(document)
 
     val sentences: List[CoreMap] = document.get(classOf[SentencesAnnotation]).asScala.toList
-    val tokensAndEntities = for {
-      sentence: CoreMap <- sentences
-      token: CoreLabel <- sentence.get(classOf[TokensAnnotation]).asScala.toList
-      word: String = token.get(classOf[TextAnnotation])
-    } yield {
-      (sentence, token, word)
-    }
 
-    tokensAndEntities
-      .groupBy { case (sentence, _, _) => sentence }
-      .map { case (_, sentenceAndTokens) => sentenceAndTokens.head }.toList
-      .map { case (sentence, token, word) => WordInSentence(sentence.toString(), word, TextRange(token.beginPosition(), token.endPosition())) }
-      .sortBy(_.range.from)
+    for {
+      sentence: CoreMap <- sentences
+    } yield {
+      val firstToken = sentence.get(classOf[TokensAnnotation]).asScala.toList.head
+      val word = firstToken.get(classOf[TextAnnotation])
+      WordInSentence(
+        sentence.toString,
+        word,
+        TextRange(firstToken.beginPosition(), firstToken.endPosition())
+      )
+    }
   }
 }
