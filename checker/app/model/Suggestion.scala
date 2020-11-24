0cbfe115cfffbfa5166d0e3c106b4912830b23d6
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-11-24 09:29:32 +0000

    Revert "RegexMatcher: address capitalisation problems at sentence starts"

--- checker/app/model/Suggestion.scala
+++ checker/app/model/Suggestion.scala
@@ -13,16 +13,6 @@ object Suggestion {
 sealed trait Suggestion {
   val `type`: String
   val text: String
-
-  /**
-    * If our suggestion is at the start of a sentence, cap up the first letter.
-    */
-  def ensureCorrectCase(isStartOfSentence: Boolean): Suggestion = this match {
-    case TextSuggestion(text) if isStartOfSentence => {
-      TextSuggestion(text = text.charAt(0).toUpper + text.slice(1, text.length))
-    }
-    case suggestion => suggestion
-  }
 }
 
 object TextSuggestion {
