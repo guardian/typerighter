d889473fb0d5c64003ba2467071e0f9f9afdec52
Sam Hession <samhession@live.co.uk>
2020-12-11 15:00:22 +0000

    Adds working rule-manager and DB stacks

---
+++ cdk/lib/rule-manager/rule-manager-db.ts
@@ -0,0 +1,106 @@
+import { Port, SecurityGroup, SubnetType } from "@aws-cdk/aws-ec2";
+import { Credentials, DatabaseInstanceEngine, PostgresEngineVersion, StorageType, SubnetGroup } from "@aws-cdk/aws-rds";
+import type { App } from "@aws-cdk/core";
+import { Fn } from "@aws-cdk/core";
+import { Duration, RemovalPolicy, SecretValue, Tags } from "@aws-cdk/core";
+import { GuSSMParameter, GuStackProps } from "@guardian/cdk/lib/constructs/core";
+import {
+  GuArnParameter,
+  GuInstanceTypeParameter,
+  GuParameter,
+  GuStack,
+  GuStringParameter,
+  GuSubnetListParameter,
+  GuVpcParameter,
+} from "@guardian/cdk/lib/constructs/core";
+import { GuSecurityGroup, GuVpc } from "@guardian/cdk/lib/constructs/ec2";
+import { GuDatabaseInstance } from "@guardian/cdk/lib/constructs/rds";
+
+export class RuleManagerDB extends GuStack {
+  constructor(scope: App, id: string, props?: GuStackProps) {
+    super(scope, id, props);
+
+    const dbPort = 5432;
+
+    const parameters = {
+      VpcId: new GuSSMParameter(this, "VpcId", {
+        description: "ID of the VPC onto which to launch the application eg. vpc-1234abcd",
+        default: "/account/vpc/default/id"
+      }),
+      PrivateVpcSubnets: new GuSSMParameter(this, "PrivateVpcSubnets", {
+        description: "Subnets to use in VPC for private EC2 instances eg. subnet-abcd1234",
+        default: "/account/vpc/default/private.subnets"
+      }),
+      DBMasterUser: new GuSSMParameter(this, "DBMasterUser", {
+        description: "Master user of the database",
+        default: `/${this.stage}/${this.stack}/typerighter-rule-manager/db.master.password`,
+      }),
+      AccessSecurityGroupID: new GuParameter(this, "AccessSecurityGroupID", {
+        description: "Id of the security group from which access to the DB will be allowed",
+        type: "AWS::EC2::SecurityGroup::Id",
+      }),
+      KMSKey: new GuArnParameter(this, "KMSKey", {
+        description: "ARN of the KMS Key to use to encrypt the database",
+      }),
+    };
+
+    /* Resources */
+
+    const vpc = GuVpc.fromId(this, "vpc", parameters.VpcId.valueAsString);
+
+    const subnets = GuVpc.subnets(this, Fn.split(',', parameters.PrivateVpcSubnets.valueAsString));
+
+    const dbSecurityGroup = new GuSecurityGroup(this, "DBSecurityGroup", {
+      description: "DB security group servers",
+      vpc,
+      allowAllOutbound: true,
+      overrideId: true,
+    });
+    
+    dbSecurityGroup.connections.allowFrom(
+      SecurityGroup.fromSecurityGroupId(this, "accessSecurityGroup", parameters.AccessSecurityGroupID.valueAsString),
+      Port.tcp(dbPort)
+    );
+
+    const dbInstance = new GuDatabaseInstance(this, "RuleManagerRDS", {
+      vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE },
+      allocatedStorage: 50,
+      allowMajorVersionUpgrade: true,
+      autoMinorVersionUpgrade: true,
+      backupRetention: Duration.days(10),
+      engine: DatabaseInstanceEngine.postgres({
+        version: PostgresEngineVersion.VER_11_8,
+      }),
+      instanceType: "t3.micro",
+      instanceIdentifier: `typerighter-rule-manager-db-${this.stage}`,
+      parameters: {
+        max_connections: "100",
+        maintenance_work_mem: "245760",
+        work_mem: "38912",
+        checkpoint_completion_target: "0.9",
+        shared_buffers: "131072",
+        synchronous_commit: "off",
+        effective_cache_size: "393216",
+        random_page_cost: "1.1",
+      },
+      subnetGroup: new SubnetGroup(this, "DBSubnetGroup", {
+        vpc,
+        vpcSubnets: { subnets },
+        description: "Private subnet for typerighter rule-manager database",
+      }),
+      credentials: Credentials.fromPassword(
+        parameters.DBMasterUser.valueAsString, 
+        SecretValue.ssmSecure(`/${this.stage}/${this.stack}/typerighter-rule-manager/db.master.password`, "1")
+      ),
+      multiAz: true,
+      port: dbPort,
+      preferredBackupWindow: "02:00-02:30",
+      preferredMaintenanceWindow: "Mon:06:30-Mon:07:00",
+      securityGroups: [dbSecurityGroup],
+      storageEncrypted: true,
+      storageType: StorageType.GP2,
+      removalPolicy: RemovalPolicy.SNAPSHOT
+    });
+  }
+}
--- cdk/lib/rule-manager/rule-manager.ts
+++ cdk/lib/rule-manager/rule-manager.ts
@@ -1,26 +1,132 @@
+// TODO: How do we want to do imports for both our own components and cdk?
+import { HealthCheck } from "@aws-cdk/aws-autoscaling";
+import { ApplicationProtocol, ListenerAction, Protocol, TargetType } from "@aws-cdk/aws-elasticloadbalancingv2";
+import type { App } from "@aws-cdk/core";
+import { Duration, Tags } from "@aws-cdk/core";
+import { InstanceRole } from "@guardian/cdk";
+import { GuAutoScalingGroup } from "@guardian/cdk/lib/constructs/autoscaling";
 import {
-  GuStack,
+  GuArnParameter,
+  GuParameter,
+  GuSSMParameter,
+  GuStringParameter,
   GuSubnetListParameter,
   GuVpcParameter,
-  GuAmiParameter
 } from "@guardian/cdk/lib/constructs/core";
-import { App, StackProps } from '@aws-cdk/core';
+import type { GuStackProps } from "@guardian/cdk/lib/constructs/core/stack";
+import { GuStack } from "@guardian/cdk/lib/constructs/core/stack";
+import { GuSecurityGroup, GuVpc } from "@guardian/cdk/lib/constructs/ec2";
+import {
+  GuApplicationListener,
+  GuApplicationLoadBalancer,
+  GuApplicationTargetGroup,
+} from "@guardian/cdk/lib/constructs/loadbalancing";
 
+// TODO: Can we pass app in as a prop?
+// TODO: Can we do the same for Stage and Stack? How does that work if sometimes they're
+//       parameters and other times they're hardcoded
+// TODO: Setup snapshot tests to give us diffs when things change
+// TODO: 
 export class RuleManager extends GuStack {
-  constructor(scope: App, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: GuStackProps) {
     super(scope, id, props);
 
     const parameters = {
-      Subnets: new GuSubnetListParameter(this, "Subnets", {
-        description: "The subnets where AMIable instances will run",
+      VPC: new GuVpcParameter(this, "VPC", {
+        description: "Virtual Private Cloud to run EC2 instances within",
       }),
-      VpcId: new GuVpcParameter(this, "VpcId", {
-        description: "The VPC in which AMIable instances will run",
-        default: "/account/vpc/default/id"
+      PublicSubnets: new GuSubnetListParameter(this, "PublicSubnets", {
+        description: "Subnets to run load balancer within",
       }),
-      AMI: new GuAmiParameter(this, "AMI", {
-        description: "AMI to use for instances",
+      PrivateSubnets: new GuSubnetListParameter(this, "PrivateSubnets", {
+        description: "Subnets to run the ASG and instances within",
       }),
-    }
+      TLSCert: new GuArnParameter(this, "TLSCert", {
+        description: "ARN of a TLS certificate to install on the load balancer",
+      }),
+      AMI: new GuStringParameter(this, "AMI", {
+        description: "AMI ID",
+      }),
+      ClusterName: new GuStringParameter(this, "ClusterName", {
+        description: "The value of the ElasticSearchCluster tag that this instance should join",
+        default: "elk",
+      }),
+    };
+
+    Tags.of(this).add("ElasticSearchCluster", parameters.ClusterName.valueAsString);
+
+    const vpc = GuVpc.fromId(this, "vpc", parameters.VPC.valueAsString);
+
+    const ruleManagerRole = new InstanceRole(this, {
+      artifactBucket: "composer-dist"
+    });
+
+    const targetGroup = new GuApplicationTargetGroup(this, "GrafanaInternalTargetGroup", {
+      vpc: vpc,
+      port: 3000,
+      protocol: ApplicationProtocol.HTTP,
+      targetType: TargetType.INSTANCE,
+      healthCheck: {
+        port: "3000",
+        protocol: Protocol.HTTP,
+        path: "/api/health",
+        interval: Duration.minutes(1),
+        timeout: Duration.seconds(3),
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 5,
+      },
+      deregistrationDelay: Duration.seconds(30),
+    });
+
+    // TODO: we should be able to remove this, as the consuming code should be able to provide a default
+    const loadBalancerSecurityGroup = new GuSecurityGroup(this, "LoadBalancerSecurityGroup", {
+      description: "Guardian IP range has access to the load balancer on port 80",
+      vpc: vpc,
+      allowAllOutbound: false
+    });
+
+    const subnets = GuVpc.subnets(this, parameters.PrivateSubnets.valueAsList);
+
+    const loadBalancer = new GuApplicationLoadBalancer(this, "InternalLoadBalancer", {
+      vpc,
+      internetFacing: true,
+      vpcSubnets: { subnets },
+      securityGroup: loadBalancerSecurityGroup,
+    });
+
+    new GuApplicationListener(this, "GrafanaInternalListener", {
+      loadBalancer,
+      certificates: [{ certificateArn: parameters.TLSCert.valueAsString }],
+      defaultAction: ListenerAction.forward([targetGroup]),
+      open: false,
+    });
+
+    // TODO: we should be able to remove this, as the consuming code should be able to provide a default
+    const appSecurityGroup = new GuSecurityGroup(this, "ApplicationSecurityGroup", {
+      description: "HTTP",
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    const userData = `#!/bin/bash -ev`;
+
+    // TODO: ASG used to have `AvailabilityZones: !GetAZs ''`
+    // TODO: Maybe there's a nicer way of doing the security groups than this
+    new GuAutoScalingGroup(this, "AutoscalingGroup", {
+      vpc,
+      vpcSubnets: { subnets },
+      role: ruleManagerRole,
+      imageId: parameters.AMI.valueAsString,
+      userData: userData,
+      instanceType: "t3.micro",
+      minCapacity: 1,
+      maxCapacity: 2,
+      healthCheck: HealthCheck.elb({
+        grace: Duration.minutes(5),
+      }),
+      targetGroup,
+      securityGroup: appSecurityGroup,
+      associatePublicIpAddress: false,
+    });
   }
 }
