37cdf42ea57686056b2ab1ead126ca1abff95e86
TJ Silver <tjsilver@protonmail.com>
2020-12-14 15:55:01 +0000

    add Source button component

--- apps/rule-manager/rule-manager-client/src/components/App.tsx
+++ apps/rule-manager/rule-manager-client/src/components/App.tsx
@@ -1,16 +1,24 @@
 /** @jsx jsx */
 import { text, background } from "@guardian/src-foundations/palette";
-import { jsx, css } from "@emotion/react";
+import { jsx, css } from "@emotion/core";
 import { Fragment } from "react";
+import { Button } from "@guardian/src-button";
 
 const headline = css`
   color: ${text.error};
   background: ${background.secondary};
-`
+`;
 
+const action = () => console.log("button clicked");
 const App = () => (
   <Fragment>
     <div css={headline}>Hello world! This is the rule manager client!</div>
+    <Button
+    priority="primary"
+    size="default"
+    iconSide="left"
+    hideLabel={false}
+    onClick={action}>Click me!</Button>
   </Fragment>
 );
 
