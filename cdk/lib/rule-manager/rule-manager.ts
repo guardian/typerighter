54dc18dc77d48c9e75eeae51aeb42578e25881c1
Sam Hession <samhession@live.co.uk>
2020-12-15 16:39:24 +0000

    Adds database url parameter to stack

--- cdk/lib/rule-manager/rule-manager.ts
+++ cdk/lib/rule-manager/rule-manager.ts
@@ -60,6 +60,9 @@ export class RuleManager extends GuStack {
         description: "The value of the ElasticSearchCluster tag that this instance should join",
         default: "elk",
       }),
+      DatabaseURL: new GuStringParameter(this, "DatabaseURL", {
+        description: "URL of the RDS DB",
+      })
     };
 
     Tags.of(this).add("ElasticSearchCluster", parameters.ClusterName.valueAsString);
@@ -145,6 +148,7 @@ mkdir /etc/gu
 
 cat > /etc/gu/typerighter-rule-manager.conf <<-'EOF'
     include "application"
+    db.default.url="jdbc:postgresql://${parameters.DatabaseURL.value}/postgres"
 EOF
 
 aws --quiet --region ${this.region} s3 cp s3://composer-dist/${this.stack}/${this.stage}/typerighter-rule-manager/typerighter-rule-manager.deb /tmp/package.deb
