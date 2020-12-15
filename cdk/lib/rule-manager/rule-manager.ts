f4c81b0c47041efba40c034da85a6e014ee3ef2b
Sam Hession <samhession@live.co.uk>
2020-12-15 13:46:00 +0000

    Adds .conf file to user data script

--- cdk/lib/rule-manager/rule-manager.ts
+++ cdk/lib/rule-manager/rule-manager.ts
@@ -115,6 +115,12 @@ export class RuleManager extends GuStack {
     });
 
     const userData = `#!/bin/bash -ev
+    mkdir /etc/gu
+
+    cat > /etc/gu/typerighter-rule-manager.conf <<-'EOF'
+    include "application"
+    EOF
+
     aws --quiet --region ${this.region} s3 cp s3://composer-dist/${this.stack}/${this.stage}/typerighter-rule-manager/typerighter-rule-manager.deb /tmp/package.deb
     dpkg -i /tmp/package.deb`;
 
