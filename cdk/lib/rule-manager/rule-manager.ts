3908e96d236a02e5aa424981aba036f5ef875524
Sam Hession <samhession@live.co.uk>
2020-12-15 14:11:46 +0000

    Fixes here-document indentation

--- cdk/lib/rule-manager/rule-manager.ts
+++ cdk/lib/rule-manager/rule-manager.ts
@@ -115,14 +115,14 @@ export class RuleManager extends GuStack {
     });
 
     const userData = `#!/bin/bash -ev
-    mkdir /etc/gu
+mkdir /etc/gu
 
-    cat > /etc/gu/typerighter-rule-manager.conf <<-'EOF'
+cat > /etc/gu/typerighter-rule-manager.conf <<-'EOF'
     include "application"
-    EOF
+EOF
 
-    aws --quiet --region ${this.region} s3 cp s3://composer-dist/${this.stack}/${this.stage}/typerighter-rule-manager/typerighter-rule-manager.deb /tmp/package.deb
-    dpkg -i /tmp/package.deb`;
+aws --quiet --region ${this.region} s3 cp s3://composer-dist/${this.stack}/${this.stage}/typerighter-rule-manager/typerighter-rule-manager.deb /tmp/package.deb
+dpkg -i /tmp/package.deb`;
 
     // TODO: ASG used to have `AvailabilityZones: !GetAZs ''`
     // TODO: Maybe there's a nicer way of doing the security groups than this
