304151b0e0b3810fb94d7358c32fa965b9ac2f38
TJ Silver <tjsilver@protonmail.com>
2020-12-23 16:34:14 +0000

    upgrade to webpack 5

--- apps/rule-manager/rule-manager-client/conf/webpack.common.config.js
+++ apps/rule-manager/rule-manager-client/conf/webpack.common.config.js
@@ -52,11 +52,7 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        type: 'asset/resource'
       },
     ]
   },
