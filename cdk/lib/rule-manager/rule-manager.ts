26e0b2c3b7904476b3dac7a15af69ed1affd3dc5
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-09-06 12:45:59 +0100

    Remove unneeded conf file from cdk

--- cdk/lib/rule-manager/rule-manager.ts
+++ cdk/lib/rule-manager/rule-manager.ts
@@ -127,12 +127,6 @@ export class RuleManager extends GuStack {
     appSecurityGroup.connections.allowFrom(loadBalancer, Port.tcp(9000), "Port 9000 LB to fleet");
 
     const userData = `#!/bin/bash -ev
-mkdir /etc/gu
-
-cat > /etc/gu/typerighter-rule-manager.conf <<-'EOF'
-    include "application"
-EOF
-
 aws --quiet --region ${this.region} s3 cp s3://composer-dist/${this.stack}/${this.stage}/typerighter-rule-manager/typerighter-rule-manager.deb /tmp/package.deb
 dpkg -i /tmp/package.deb`;
 
