7934f17a5cfbd4b2d66ed8b04ac756ef2fc0fbf4
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2021-09-10 13:42:48 +0100

    Limit outgoing traffic of rule manager ec2 instance

--- cdk/lib/rule-manager/rule-manager.ts
+++ cdk/lib/rule-manager/rule-manager.ts
@@ -3,13 +3,17 @@ import type { GuStackProps } from "@guardian/cdk/lib/constructs/core/stack";
 import { GuStack } from "@guardian/cdk/lib/constructs/core/stack";
 import { AccessScope, GuPlayApp } from "@guardian/cdk";
 import { GuGetS3ObjectsPolicy } from "@guardian/cdk/lib/constructs/iam";
+import { GuSecurityGroup } from "@guardian/cdk/lib/constructs/ec2";
+import { Peer } from "@aws-cdk/aws-ec2";
 
 export class RuleManager extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
 
-    new GuPlayApp(this, {
-      app: "typerighter-rule-manager",
+    const appName = "typerighter-rule-manager";
+
+    const app = new GuPlayApp(this, {
+      app: appName,
       userData: `#!/bin/bash -ev
         aws --quiet --region ${this.region} s3 cp s3://composer-dist/${this.stack}/${this.stage}/typerighter-rule-manager/typerighter-rule-manager.deb /tmp/package.deb
         dpkg -i /tmp/package.deb`,
@@ -31,5 +35,23 @@ export class RuleManager extends GuStack {
         ],
       },
     });
+
+    const dbAccessSecurityGroup = new GuSecurityGroup(
+      this,
+      "ApplicationSecurityGroup",
+      {
+        app: appName,
+        description: "Application security group",
+        vpc: app.vpc,
+        allowAllOutbound: false,
+        egresses: [{
+          range: Peer.anyIpv4(),
+          port: 5432,
+          description: "Allows application EC2 instance to access postgres DB"
+        }]
+      }
+    );
+
+    app.autoScalingGroup.addSecurityGroup(dbAccessSecurityGroup);
   }
 }
