4e69e560e5e577898637ee626a90d1c0f50db49e
Rhys Mills <rhystmills@gmail.com>
2021-12-16 14:58:29 +0000

    Bump Postgres engine version in cdk

--- cdk/lib/rule-manager/rule-manager-db.ts
+++ cdk/lib/rule-manager/rule-manager-db.ts
@@ -1,12 +1,15 @@
 import { Port, SecurityGroup, SubnetType } from "@aws-cdk/aws-ec2";
-import { Credentials, DatabaseInstanceEngine, PostgresEngineVersion, StorageType, SubnetGroup } from "@aws-cdk/aws-rds";
+import {
+  Credentials,
+  DatabaseInstanceEngine,
+  PostgresEngineVersion,
+  StorageType,
+  SubnetGroup,
+} from "@aws-cdk/aws-rds";
 import type { App } from "@aws-cdk/core";
 import { Duration, RemovalPolicy, SecretValue, Tags } from "@aws-cdk/core";
 import { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import {
-  GuParameter,
-  GuStack
-} from "@guardian/cdk/lib/constructs/core";
+import { GuParameter, GuStack } from "@guardian/cdk/lib/constructs/core";
 import { GuSecurityGroup, GuVpc } from "@guardian/cdk/lib/constructs/ec2";
 import { GuDatabaseInstance } from "@guardian/cdk/lib/constructs/rds";
 export class RuleManagerDB extends GuStack {
@@ -19,29 +22,35 @@ export class RuleManagerDB extends GuStack {
       VpcId: new GuParameter(this, "VPC", {
         type: "AWS::SSM::Parameter::Value<AWS::EC2::VPC::Id>",
         description: "Virtual Private Cloud to run EC2 instances within",
-        default: "/account/vpc/default/id"
+        default: "/account/vpc/default/id",
       }),
       MasterDBUsername: new GuParameter(this, "MasterDBUsername", {
         description: "Master DB username",
         default: "rule_manager",
-        type: "String"
+        type: "String",
       }),
       PrivateVpcSubnets: new GuParameter(this, "PrivateSubnets", {
         type: "AWS::SSM::Parameter::Value<List<AWS::EC2::Subnet::Id>>",
         description: "Subnets to run the ASG and instances within",
-        default: "/account/vpc/default/private.subnets"
+        default: "/account/vpc/default/private.subnets",
       }),
       AccessSecurityGroupID: new GuParameter(this, "AccessSecurityGroupID", {
-        description: "Id of the security group from which access to the DB will be allowed",
+        description:
+          "Id of the security group from which access to the DB will be allowed",
         type: "AWS::EC2::SecurityGroup::Id",
-      })
+      }),
     };
 
     /* Resources */
 
-    const vpc = GuVpc.fromId(this, "Vpc", { vpcId: parameters.VpcId.valueAsString });
+    const vpc = GuVpc.fromId(this, "Vpc", {
+      vpcId: parameters.VpcId.valueAsString,
+    });
 
-    const subnets = GuVpc.subnets(this, parameters.PrivateVpcSubnets.valueAsList);
+    const subnets = GuVpc.subnets(
+      this,
+      parameters.PrivateVpcSubnets.valueAsList
+    );
 
     const dbSecurityGroup = new GuSecurityGroup(this, "DBSecurityGroup", {
       description: "DB security group servers",
@@ -51,7 +60,11 @@ export class RuleManagerDB extends GuStack {
     });
 
     dbSecurityGroup.connections.allowFrom(
-      SecurityGroup.fromSecurityGroupId(this, "AccessSecurityGroup", parameters.AccessSecurityGroupID.valueAsString),
+      SecurityGroup.fromSecurityGroupId(
+        this,
+        "AccessSecurityGroup",
+        parameters.AccessSecurityGroupID.valueAsString
+      ),
       Port.tcp(dbPort)
     );
 
@@ -63,9 +76,9 @@ export class RuleManagerDB extends GuStack {
       autoMinorVersionUpgrade: true,
       backupRetention: Duration.days(10),
       engine: DatabaseInstanceEngine.postgres({
-        version: PostgresEngineVersion.VER_11,
+        version: PostgresEngineVersion.of("13.3", "13", { s3Import: true }), // FIXME: Use PostgresEngineVersion.VER_13_3 once CDK dependencies are bumped
       }),
-      instanceType: "t3.micro",
+      instanceType: "t3.micro", // TODO
       instanceIdentifier: `typerighter-rule-manager-db-${this.stage}`,
       parameters: {
         max_connections: "100",
@@ -84,7 +97,10 @@ export class RuleManagerDB extends GuStack {
       }),
       credentials: Credentials.fromPassword(
         parameters.MasterDBUsername.valueAsString,
-        SecretValue.ssmSecure(`/${this.stage}/${this.stack}/typerighter-rule-manager/db.default.password`, "1")
+        SecretValue.ssmSecure(
+          `/${this.stage}/${this.stack}/typerighter-rule-manager/db.default.password`,
+          "1"
+        )
       ),
       multiAz: true,
       port: dbPort,
@@ -93,7 +109,7 @@ export class RuleManagerDB extends GuStack {
       securityGroups: [dbSecurityGroup],
       storageEncrypted: true,
       storageType: StorageType.GP2,
-      removalPolicy: RemovalPolicy.SNAPSHOT
+      removalPolicy: RemovalPolicy.SNAPSHOT,
     });
   }
 }
