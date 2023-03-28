910749ea205d357984e7638399c572cbd1b5d038
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-03-28 11:55:39 +0100

    Increase timeout for GHA

--- apps/checker/test/scala/services/MatcherPoolTest.scala
+++ apps/checker/test/scala/services/MatcherPoolTest.scala
@@ -103,7 +103,7 @@ class MatcherPoolTest extends AsyncFlatSpec with Matchers {
     maxCurrentJobs: Int = 4,
     maxQueuedJobs: Int = 100,
     strategy: MatcherPool.CheckStrategy = MatcherPool.documentPerCategoryCheckStrategy,
-    checkTimeoutDuration: FiniteDuration = 100 milliseconds
+    checkTimeoutDuration: FiniteDuration = 500 milliseconds
   ): MatcherPool = {
     val futures = new DefaultFutures(system)
     val pool = new MatcherPool(maxCurrentJobs, maxQueuedJobs, strategy, futures, checkTimeoutDuration = checkTimeoutDuration)
