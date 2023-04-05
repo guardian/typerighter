f87e4f5ca5045544ec5087af52ebe92f9aadc8b6
Rhys Mills <rhystmills@gmail.com>
2023-04-05 16:11:33 +0100

    Add an endpoint to create a rule

--- apps/rule-manager/app/controllers/RulesController.scala
+++ apps/rule-manager/app/controllers/RulesController.scala
@@ -3,7 +3,10 @@ package controllers
 import com.gu.pandomainauth.PublicSettings
 import com.gu.typerighter.lib.PandaAuthentication
 import com.gu.typerighter.rules.{BucketRuleManager, SheetsRuleManager}
-import play.api.libs.json.Json
+import db.DbRule
+import model.{CreateRuleForm, UpdateRuleForm}
+import play.api.data.FormError
+import play.api.libs.json.{JsObject, JsValue, Json, Writes}
 import play.api.mvc._
 import service.DbRuleManager
 
@@ -36,6 +39,59 @@ class RulesController(
     bucketRuleManager.getRules() match {
       case Right((ruleResource, _)) => Ok(Json.toJson(ruleResource))
       case Left(error)              => InternalServerError(Json.toJson(error.getMessage))
+    }
+  }
+
+  implicit object FormErrorWrites extends Writes[FormError] {
+    override def writes(o: FormError): JsValue = Json.obj(
+      "key" -> Json.toJson(o.key),
+      "message" -> Json.toJson(o.message)
+    )
+  }
+
+  def create = ApiAuthAction { implicit request: Request[AnyContent] =>
+//    val maybeJson = request.body.asJson.toRight("JSON parse failed");
+//    val maybeMap = maybeJson.flatMap(json => json match {
+//      case JsObject(fields) => Right(fields.toMap)
+//      case _ => Left("Expected JSON Object")
+//    })
+    CreateRuleForm.form.bindFromRequest.fold(
+      formWithErrors => {
+          val errors = formWithErrors.errors
+          BadRequest(Json.toJson(errors))
+        },
+      formRule => {
+        val dbRule = DbRule.create(
+          formRule.ruleType,
+          formRule.pattern,
+          formRule.replacement,
+          formRule.category,
+          formRule.tags,
+          formRule.description,
+          formRule.ignore,
+          formRule.notes,
+          formRule.googleSheetId,
+          formRule.forceRedRule,
+          formRule.advisoryRule,
+        )
+        val returnedRule = DbRule.find(dbRule.id.get).get
+        val baseReturnedRule = DbRuleManager.dbRuleToBaseRule(returnedRule)
+        baseReturnedRule match {
+          case Right(rule) => Ok(Json.toJson(rule))
+          case Left(error) => InternalServerError(error)
+        }
+      }
+    )
+
+    val maybeCreateRule = for {
+      sheetRules <- sheetsRuleManager.getRules()
+      dbRules <- DbRuleManager.destructivelyDumpRuleResourceToDB(sheetRules)
+      _ <- bucketRuleManager.putRules(dbRules).left.map { l => List(l.toString) }
+    } yield dbRules
+
+    maybeCreateRule match {
+      case Right(ruleResource) => Ok(Json.toJson(ruleResource))
+      case Left(errors)        => InternalServerError(Json.toJson(errors))
     }
   }
 }
