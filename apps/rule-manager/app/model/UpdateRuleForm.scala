47ad454ba4986af08cb94a6174d42b211eec21e6
Rhys Mills <rhystmills@gmail.com>
2023-04-17 16:20:26 +0100

    Verify ruleType in updateRuleForm

--- apps/rule-manager/app/model/UpdateRuleForm.scala
+++ apps/rule-manager/app/model/UpdateRuleForm.scala
@@ -2,11 +2,33 @@ package model
 
 import play.api.data.Form
 import play.api.data.Forms.{boolean, mapping, nonEmptyText, number, optional, text}
+import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
+import service.DbRuleManager.RuleType
 
 object UpdateRuleForm {
+  val ruleTypeConstraint: Constraint[Option[String]] = Constraint("constraints.ruleType")({ text =>
+    val errors = text match {
+      case None                            => Nil
+      case Some(RuleType.regex)            => Nil
+      case Some(RuleType.languageToolCore) => Nil
+      case Some(RuleType.languageToolXML)  => Nil
+      case _ =>
+        Seq(
+          ValidationError(
+            s"RuleType must be one of \"${RuleType.regex}\", \"${RuleType.languageToolXML}\" or \"${RuleType.languageToolCore}\""
+          )
+        )
+    }
+    if (errors.isEmpty) {
+      Valid
+    } else {
+      Invalid(errors)
+    }
+  })
+
   val form = Form(
     mapping(
-      "ruleType" -> optional(text()),
+      "ruleType" -> optional(text()).verifying(ruleTypeConstraint),
       "pattern" -> optional(text()),
       "replacement" -> optional(text()),
       "category" -> optional(text()),
