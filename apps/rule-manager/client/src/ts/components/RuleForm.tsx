056e2d21370d16ae2b107d9d6c5057dbe27bd126
Rhys Mills <rhystmills@gmail.com>
2023-04-24 12:42:01 +0100

    Add buttons and managed create form visibility

--- apps/rule-manager/client/src/ts/components/RuleForm.tsx
+++ apps/rule-manager/client/src/ts/components/RuleForm.tsx
@@ -1,15 +1,29 @@
-import { EuiFlexGroup, EuiForm } from "@elastic/eui";
-import React from "react"
+import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiForm, EuiSpacer } from "@elastic/eui";
+import React, { useReducer, useState } from "react"
 import { RuleContent } from "./RuleContent";
 import { RuleType } from "./RuleType";
 import {RuleMetadata} from "./RuleMetadata";
 
 export const RuleForm = () => {
+    const [createRuleFormOpen, setCreateRuleFormOpen] = useState(false);
+    const openCreateRuleForm = () => {
+        setCreateRuleFormOpen(true);
+    }
     return <EuiForm component="form">
-            <EuiFlexGroup  direction="column">   
-                <RuleContent />
-                <RuleType />
-                <RuleMetadata />
-        </EuiFlexGroup>
+        <EuiButton isDisabled={createRuleFormOpen} onClick={openCreateRuleForm}>Create Rule</EuiButton>
+        <EuiSpacer />
+        {createRuleFormOpen ? <EuiFlexGroup  direction="column">   
+            <RuleContent />
+            <RuleType />
+            <RuleMetadata />
+            <EuiFlexGroup>
+                <EuiFlexItem>
+                    <EuiButton onClick={() => setCreateRuleFormOpen(false)}>Discard Rule</EuiButton>
+                </EuiFlexItem>
+                <EuiFlexItem>
+                    <EuiButton fill={true}>Save Rule</EuiButton>
+                </EuiFlexItem>
+            </EuiFlexGroup>
+        </EuiFlexGroup> : null}
     </EuiForm>
 }
