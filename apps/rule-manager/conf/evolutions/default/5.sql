d4cc678b6504a29211ffe065684e8fcd5b371373
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-04-26 18:40:25 +0100

    Add unique index to externalId table
    for quick lookup and data integrity, externalIds should be unique

--- apps/rule-manager/conf/evolutions/default/5.sql
+++ apps/rule-manager/conf/evolutions/default/5.sql
@@ -7,7 +7,11 @@ ALTER TABLE rules
   ALTER COLUMN external_id SET DEFAULT gen_random_uuid()::text,
   ALTER COLUMN external_id SET NOT NULL;
 
+CREATE UNIQUE INDEX rules_external_id_index ON rules(external_id);
+
 -- !Downs
+
+DROP INDEX rules_external_id_index;
 
 ALTER TABLE rules
   ALTER COLUMN external_id drop NOT NULL;
