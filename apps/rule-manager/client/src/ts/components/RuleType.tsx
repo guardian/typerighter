b3a93a3e9accb60cf21fe4c671d60dc6a4367e6c
Rhys Mills <rhystmills@gmail.com>
2023-05-02 10:28:49 +0100

    Remove ambiguous/actionable radio group

--- apps/rule-manager/client/src/ts/components/RuleType.tsx
+++ apps/rule-manager/client/src/ts/components/RuleType.tsx
@@ -1,22 +1,9 @@
 import { EuiFormRow, EuiRadioGroup, EuiSwitch, EuiSwitchEvent } from "@elastic/eui";
-import { css } from "@emotion/react";
 import React, { useState } from "react"
 import { RuleFormSection } from "./RuleFormSection"
-import {LineBreak} from "./LineBreak";
 import { PartiallyUpdateRuleData, RuleFormData } from "./RuleForm";
 
 export const RuleType = ({ruleData, partiallyUpdateRuleData}: {ruleData: RuleFormData, partiallyUpdateRuleData: PartiallyUpdateRuleData}) => {
-    const ruleTypeOptions = [
-        {
-            id: "actionable",
-            label: 'Actionable (Amend or OK)',
-        },
-        {
-            id: "ambiguous",
-            label: 'Ambiguous (Review)',
-        },
-    ]
-    const [ruleTypeSelected, setRuleTypeSelected] = useState(ruleTypeOptions[0].id);
     const [isAdvisory, setIsAdvisory] = useState(false);
 
     const onIsAdvisoryChange = (e: EuiSwitchEvent) => {
@@ -25,24 +12,6 @@ export const RuleType = ({ruleData, partiallyUpdateRuleData}: {ruleData: RuleFor
     };
     
     return <RuleFormSection title="RULE TYPE">
-        <LineBreak/>
-
-        <EuiRadioGroup
-            options={ruleTypeOptions}
-            idSelected={ruleTypeSelected}
-            onChange={(id)=> {
-                setRuleTypeSelected(id)
-            }}
-            css={css`
-                flex-direction: row;
-                display: flex;
-                gap: 1rem;
-                align-items: flex-end;
-            `}
-        />
-
-        <LineBreak/>
-
         <EuiFormRow
             helpText="Flag only once per session"
         >
