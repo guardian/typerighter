ef1dac200258ebc2b11bf839d4a3bbeeb5ef79e7
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-05-04 15:28:25 +0100

    Add profile menu to header, with feature switches UI

--- apps/rule-manager/client/src/ts/components/layout/Page.tsx
+++ apps/rule-manager/client/src/ts/components/layout/Page.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { EuiPageTemplate, EuiProvider } from "@elastic/eui";
-import {Header} from "./Header";
-import {Rules} from "../pages/Rules";
-import {euiThemeOverrides} from "../../constants/euiTheme";
+import { Header } from "./Header";
+import { Rules } from "../pages/Rules";
+import { euiThemeOverrides } from "../../constants/euiTheme";
 
-import createCache from '@emotion/cache'
+import createCache from "@emotion/cache";
+import { FeatureSwitchesProvider } from "../context/featureSwitches";
 
 // Necessary while SASS and Emotion styles coexist within EUI.
 const cache = createCache({
-  key: 'eui',
+  key: "eui",
   // Ensure SASS global styles override Emotion.
-  prepend: true
-})
+  prepend: true,
+});
 
-export const Page = () =>
-  <EuiProvider modify={euiThemeOverrides} cache={cache}>
-    <EuiPageTemplate>
-      <Header />
-      <EuiPageTemplate.Section color="subdued">
-        <Rules />
-      </EuiPageTemplate.Section>
-    </EuiPageTemplate>
-  </EuiProvider>
+export const Page = () => (
+  <FeatureSwitchesProvider>
+    <EuiProvider modify={euiThemeOverrides} cache={cache}>
+      <EuiPageTemplate>
+        <Header />
+        <EuiPageTemplate.Section color="subdued">
+          <Rules />
+        </EuiPageTemplate.Section>
+      </EuiPageTemplate>
+    </EuiProvider>
+  </FeatureSwitchesProvider>
+);
---
+++ apps/rule-manager/client/src/ts/components/layout/ProfileMenu.tsx
@@ -0,0 +1,66 @@
+import {
+  EuiBasicTable,
+  EuiBasicTableColumn,
+  EuiContextMenu,
+  EuiSwitch,
+} from "@elastic/eui";
+import React, { useContext } from "react";
+import {
+  FeatureSwitch,
+  FeatureSwitchesContext,
+} from "../context/featureSwitches";
+
+export const ProfileMenu = () => {
+  const { featureSwitches, toggleFeatureSwitch, getFeatureSwitchValue } =
+    useContext(FeatureSwitchesContext);
+
+  const featureColumns: Array<EuiBasicTableColumn<FeatureSwitch>> = [
+    {
+      field: "name",
+      name: "Feature",
+    },
+    {
+      field: "active",
+      name: "Active",
+      render: (_, featureSwitch) =>
+        getFeatureSwitchValue(featureSwitch.id).toString(),
+    },
+    {
+      field: "override",
+      name: "Override",
+      render: (_, featureSwitch) => (
+        <EuiSwitch
+          label=""
+          checked={featureSwitch.overridden}
+          onChange={() => toggleFeatureSwitch(featureSwitch.id)}
+        />
+      ),
+    },
+  ];
+  const panels = [
+    {
+      id: 0,
+      items: [
+        {
+          name: "Logout",
+          icon: "exit",
+          onClick: () => (window.location.href = "/logout"),
+        },
+        {
+          name: "Feature switches",
+          icon: "starEmptySpace",
+          panel: 1,
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: "Feature switches",
+      content: (
+        <EuiBasicTable columns={featureColumns} items={featureSwitches} />
+      ),
+    },
+  ];
+
+  return <EuiContextMenu initialPanelId={0} panels={panels} />;
+};
