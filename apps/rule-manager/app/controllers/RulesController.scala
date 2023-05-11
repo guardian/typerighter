9be561b664a53f4758affa3109e5fd398467f115
Rhys Mills <rhys_mills@31752.gnm.int>
2023-05-11 15:23:32 +0100

    Check for permissions before creating or editing rules on backend

--- apps/rule-manager/app/controllers/RulesController.scala
+++ apps/rule-manager/app/controllers/RulesController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import com.gu.pandomainauth.PublicSettings
+import com.gu.permissions.PermissionDefinition
 import com.gu.typerighter.lib.PandaAuthentication
 import com.gu.typerighter.rules.BucketRuleManager
 
@@ -12,6 +13,11 @@ import play.api.data.FormError
 import play.api.libs.json.{JsValue, Writes}
 import play.api.mvc._
 import service.{DbRuleManager, SheetsRuleManager}
+<<<<<<< HEAD
+=======
+import utils.{PermissionsHandler, RuleManagerConfig}
+
+>>>>>>> 31ceff7 (Check for permissions before creating or editing rules on backend)
 import scala.util.{Failure, Success}
 
 /** The controller that handles the management of matcher rules.
@@ -20,9 +26,11 @@ class RulesController(
     cc: ControllerComponents,
     sheetsRuleManager: SheetsRuleManager,
     bucketRuleManager: BucketRuleManager,
-    val publicSettings: PublicSettings
+    val publicSettings: PublicSettings,
+    override val config: RuleManagerConfig
 ) extends AbstractController(cc)
-    with PandaAuthentication {
+    with PandaAuthentication
+    with PermissionsHandler {
   def refresh = ApiAuthAction {
     val maybeWrittenRules = for {
       dbRules <- sheetsRuleManager.getRules()
@@ -57,37 +65,46 @@ class RulesController(
     )
   }
 
-  def create = ApiAuthAction { implicit request =>
-    CreateRuleForm.form
-      .bindFromRequest()
-      .fold(
-        formWithErrors => {
-          val errors = formWithErrors.errors
-          BadRequest(Json.toJson(errors))
-        },
-        formRule => {
-          DbRuleDraft.createFromFormRule(formRule, request.user.email) match {
-            case Success(rule)  => Ok(Json.toJson(rule))
-            case Failure(error) => InternalServerError(error.getMessage())
-          }
-        }
-      )
+  def create = ApiAuthAction { implicit request => {
+    hasPermission(request.user, PermissionDefinition("manage_rules", "typerighter")) match {
+      case false => Unauthorized("You don't have permission to create rules")
+      case true =>
+        CreateRuleForm.form
+          .bindFromRequest()
+          .fold(
+            formWithErrors => {
+              val errors = formWithErrors.errors
+              BadRequest(Json.toJson(errors))
+            },
+            formRule => {
+              DbRuleDraft.createFromFormRule(formRule, request.user.email) match {
+                case Success(rule) => Ok(Json.toJson(rule))
+                case Failure(error) => InternalServerError(error.getMessage())
+              }
+            }
+          )
+      }
+    }
   }
 
   def update(id: Int) = ApiAuthAction { implicit request =>
-    UpdateRuleForm.form
-      .bindFromRequest()
-      .fold(
-        formWithErrors => {
-          val errors = formWithErrors.errors
-          BadRequest(Json.toJson(errors))
-        },
-        formRule => {
-          DbRuleDraft.updateFromFormRule(formRule, id, request.user.email) match {
-            case Left(result)  => result
-            case Right(dbRule) => Ok(Json.toJson(dbRule))
-          }
-        }
-      )
+    hasPermission(request.user, PermissionDefinition("manage_rules", "typerighter")) match {
+      case false => Unauthorized("You don't have permission to edit rules")
+      case true =>
+        UpdateRuleForm.form
+          .bindFromRequest()
+          .fold(
+            formWithErrors => {
+              val errors = formWithErrors.errors
+              BadRequest(Json.toJson(errors))
+            },
+            formRule => {
+              DbRuleDraft.updateFromFormRule(formRule, id, request.user.email) match {
+                case Left(result) => result
+                case Right(dbRule) => Ok(Json.toJson(dbRule))
+              }
+            }
+          )
+    }
   }
 }
