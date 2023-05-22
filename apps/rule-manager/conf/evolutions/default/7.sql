752afeb66d5bf75f6e458cfe69d569d48c525a44
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-05-22 14:57:16 +0100

    Remove uniqueness constraint for rule_order, in favour of unique constraint for rule_order when is_active is true

--- apps/rule-manager/conf/evolutions/default/7.sql
+++ apps/rule-manager/conf/evolutions/default/7.sql
@@ -2,18 +2,24 @@
 
 ALTER TABLE rules_live
   DROP COLUMN id, -- We no longer use id as a primary key, so it's no longer necessary
-  ADD COLUMN rule_order INT NOT NULL UNIQUE,
+  ADD COLUMN rule_order INT NOT NULL,
   ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT FALSE;
 
 CREATE UNIQUE INDEX rules_live_is_active ON rules_live (external_id, is_active) where is_active = true;
+CREATE UNIQUE INDEX rules_live_unique_order ON rules_live (rule_order, is_active) where is_active = true;
 CREATE UNIQUE INDEX rules_live_composite_pkey ON rules_live (external_id, revision_id);
+
+DROP INDEX rules_live_external_id_index;
 
 -- !Downs
 
 DROP INDEX rules_live_composite_pkey;
 DROP INDEX rules_live_is_active;
+DROP INDEX rules_live_unique_order;
 
 ALTER TABLE rules_live
   ADD COLUMN id SERIAL PRIMARY KEY,
   DROP COLUMN is_active,
   DROP COLUMN rule_order;
+
+CREATE UNIQUE INDEX rules_live_external_id_index ON rules_live(external_id);
