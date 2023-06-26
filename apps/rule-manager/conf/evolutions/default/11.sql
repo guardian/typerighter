b4a8a90c1fd520ce52a322a4953e4496f622c9a6
Rhys Mills <rhys.mills@guardian.co.uk>
2023-06-26 09:17:56 +0100

    Get DB evolution working

--- apps/rule-manager/conf/evolutions/default/11.sql
+++ apps/rule-manager/conf/evolutions/default/11.sql
@@ -1,14 +1,17 @@
 -- !Ups
 
 ALTER TABLE rules_draft DROP COLUMN tags;
-ALTER TABLE rules_live DROP COLUMN tags;
+ALTER TABLE rules_live
+    DROP COLUMN tags;
 
 ALTER TABLE rule_tag_draft
     ADD CONSTRAINT fk_rule_id FOREIGN KEY (rule_id) REFERENCES rules_draft(id),
     ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tags(id);
 
 ALTER TABLE rule_tag_live
-    ADD CONSTRAINT fk_rule_id FOREIGN KEY (rule_id) REFERENCES rules_live(id),
+    ALTER COLUMN rule_id SET DATA TYPE TEXT,
+    ADD COLUMN revision_id INT NOT NULL DEFAULT 0,
+    ADD CONSTRAINT fk_rule_id FOREIGN KEY (rule_id, revision_id) REFERENCES rules_live(external_id, revision_id),
     ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tags(id);
 
 -- !Downs
@@ -20,3 +23,6 @@ ALTER TABLE rule_tag_draft
 ALTER TABLE rule_tag_live
     DROP CONSTRAINT fk_rule_id,
     DROP CONSTRAINT fk_tag_id;
+
+ALTER TABLE rules_live
+    DROP CONSTRAINT unique_external_id;
