56e28d0634c131eedcab64219e25fdd3cf860bfe
Samantha Gottlieb <samanthagottlieb31@gmail.com>
2023-07-04 12:48:38 +0100

    Make Description box markdown editor

--- apps/rule-manager/client/src/ts/components/RuleContent.tsx
+++ apps/rule-manager/client/src/ts/components/RuleContent.tsx
@@ -13,75 +13,98 @@ type RuleTypeOption = {
   label: string,
 }
 
-export const RuleContent = ({ruleData, partiallyUpdateRuleData, showErrors}: {
-        ruleData: DraftRule,
-        partiallyUpdateRuleData: PartiallyUpdateRuleData,
-        showErrors: boolean
-    }) => {
+const { parsingPlugins, processingPlugins, uiPlugins } =
+  getDefaultEuiMarkdownPlugins({ exclude: ["tooltip"] });
 
-    const ruleTypeOptions: RuleTypeOption[] = [
-        {
-            id: "regex",
-            label: "Regex",
-        },
-        {
-            id: "languageToolXML",
-            label: "LanguageTool",
-        },
-    ]
-    const TextField = ruleData.ruleType === "languageToolXML" ? EuiTextArea : EuiFieldText;
+export const RuleContent = ({
+  ruleData,
+  partiallyUpdateRuleData,
+  showErrors,
+}: {
+  ruleData: DraftRule;
+  partiallyUpdateRuleData: PartiallyUpdateRuleData;
+  showErrors: boolean;
+}) => {
+  const ruleTypeOptions: RuleTypeOption[] = [
+    {
+      id: "regex",
+      label: "Regex",
+    },
+    {
+      id: "languageToolXML",
+      label: "LanguageTool",
+    },
+  ];
+  const TextField =
+    ruleData.ruleType === "languageToolXML" ? EuiTextArea : EuiFieldText;
 
-    return <RuleFormSection title="RULE CONTENT" additionalInfo={
-      ruleData.updatedAt &&
-      <LastUpdated lastUpdated={ruleData.updatedAt} />
-    }>
-        <LineBreak/>
-        <EuiFlexItem>
-          <EuiFormLabel>Rule type</EuiFormLabel>
-            <EuiRadioGroup
-                options={ruleTypeOptions}
-                idSelected={ruleData.ruleType}
-                onChange={(ruleType) => {
-                    partiallyUpdateRuleData({ruleType: ruleType as RuleType});
-                }}
-                css={css`
-                        display: flex;
-                        gap: 1rem;
-                        align-items: flex-end;
-                    `}
-            />
-            <EuiSpacer size="s" />
-            <EuiFormRow
-                label={<Label text='Pattern' required={true}/>}
-                isInvalid={showErrors && !ruleData.pattern}
-                fullWidth={true}
-            >
-                <TextField
-                    value={ruleData.pattern || ""}
-                    onChange={(_ => partiallyUpdateRuleData({pattern: _.target.value}))}
-                    required={true}
-                    isInvalid={showErrors && !ruleData.pattern}
-                    fullWidth={true}
-                />
-            </EuiFormRow>
-            <EuiFormRow
-                label="Replacement"
-                helpText="What is the ideal term as per the house style?"
-                fullWidth={true}
-            >
-                <EuiFieldText value={ruleData.replacement || ""}
-                              onChange={(_ => partiallyUpdateRuleData({replacement: _.target.value}))}
-                              fullWidth={true} />
-            </EuiFormRow>
-            <EuiFormRow
-                label="Description"
-                helpText="What will the users see in Composer?"
-                fullWidth={true}
-            >
-                <EuiFieldText value={ruleData.description || ""}
-                              onChange={(_ => partiallyUpdateRuleData({description: _.target.value}))}
-                              fullWidth={true} />
-            </EuiFormRow>
-        </EuiFlexItem>
+  return (
+    <RuleFormSection
+      title="RULE CONTENT"
+      additionalInfo={
+        ruleData.updatedAt && <LastUpdated lastUpdated={ruleData.updatedAt} />
+      }
+    >
+      <LineBreak />
+      <EuiFlexItem>
+        <EuiFormLabel>Rule type</EuiFormLabel>
+        <EuiRadioGroup
+          options={ruleTypeOptions}
+          idSelected={ruleData.ruleType}
+          onChange={(ruleType) => {
+            partiallyUpdateRuleData({ ruleType: ruleType as RuleType });
+          }}
+          css={css`
+            display: flex;
+            gap: 1rem;
+            align-items: flex-end;
+          `}
+        />
+        <EuiSpacer size="s" />
+        <EuiFormRow
+          label={<Label text="Pattern" required={true} />}
+          isInvalid={showErrors && !ruleData.pattern}
+          fullWidth={true}
+        >
+          <TextField
+            value={ruleData.pattern || ""}
+            onChange={(_) =>
+              partiallyUpdateRuleData({ pattern: _.target.value })
+            }
+            required={true}
+            isInvalid={showErrors && !ruleData.pattern}
+            fullWidth={true}
+          />
+        </EuiFormRow>
+        <EuiFormRow
+          label="Replacement"
+          helpText="What is the ideal term as per the house style?"
+          fullWidth={true}
+        >
+          <EuiFieldText
+            value={ruleData.replacement || ""}
+            onChange={(_) =>
+              partiallyUpdateRuleData({ replacement: _.target.value })
+            }
+            fullWidth={true}
+          />
+        </EuiFormRow>
+        <EuiFormRow
+          label="Description"
+          helpText="What will the users see in Composer?"
+          fullWidth={true}
+        >
+          <EuiMarkdownEditor
+            value={ruleData.description || ""}
+            onChange={(value) =>
+              partiallyUpdateRuleData({ description: value })
+            }
+            aria-label={"Description editor"}
+            parsingPluginList={parsingPlugins}
+            processingPluginList={processingPlugins}
+            uiPlugins={uiPlugins}
+          />
+        </EuiFormRow>
+      </EuiFlexItem>
     </RuleFormSection>
 }
