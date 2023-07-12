7a8038726490f9a8de67c4c630c97eb3f86f3a08
Rhys Mills <rhys.mills@guardian.co.uk>
2023-07-12 14:20:24 +0100

    Add basic Tags table

---
+++ apps/rule-manager/client/src/ts/components/TagsTable.tsx
@@ -0,0 +1,43 @@
+import { EuiFlexGroup, EuiFlexItem, EuiInMemoryTable, EuiTitle } from "@elastic/eui"
+import { css } from "@emotion/react"
+import { useTags } from "./hooks/useTags";
+
+const createTagTableColumns = () => {
+    return [
+        {
+            field: 'id',
+            name: 'id'
+        },{
+            field: 'name',
+            name: 'Name'
+        }
+    ]
+}
+export const TagsTable = () => {
+    const {tags, fetchTags} = useTags();
+    return (<>
+        <EuiFlexGroup>
+            <EuiFlexItem/>
+            <EuiFlexItem>
+                <EuiFlexGroup>
+                    <EuiFlexItem grow={false} css={css`padding-bottom: 20px;`}>
+                        <EuiTitle>
+                            <h1>Tags</h1>
+                        </EuiTitle>
+                    </EuiFlexItem>
+                </EuiFlexGroup>
+                <EuiFlexGroup>
+                    <EuiFlexItem>
+                        <EuiInMemoryTable
+                            columns={createTagTableColumns()}
+                            items={Object.values(tags)}
+                            css={css`max-width: 500px`}
+                        >
+                        </EuiInMemoryTable>
+                    </EuiFlexItem>
+                </EuiFlexGroup>
+            </ EuiFlexItem>
+            <EuiFlexItem/>
+        </EuiFlexGroup>
+    </>)
+}
