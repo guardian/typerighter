39b00aea989eb6ab96dae4238e430aa2e7220ee2
Samantha Gottlieb <samanthagottlieb31@gmail.com>
2023-07-17 11:35:19 +0100

    Add missing semicolon

--- apps/rule-manager/conf/evolutions/default/12.sql
+++ apps/rule-manager/conf/evolutions/default/12.sql
@@ -4,7 +4,7 @@ ALTER TABLE rules_draft
     ADD COLUMN rule_order INT UNIQUE;
 
 UPDATE rules_draft
-    SET rule_order = id
+    SET rule_order = id;
 
 ALTER TABLE rules_draft
     ALTER COLUMN rule_order SET NOT NULL;
