bf3efd82e624bc25505bea9962d0c53b90ea9dde
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-07-17 11:28:11 +0100

    Add variable to better explain choice of char

--- apps/checker/app/utils/JsonHelpers.scala
+++ apps/checker/app/utils/JsonHelpers.scala
@@ -4,7 +4,9 @@ import java.net.URL
 import play.api.libs.json.{JsString, Json, Writes}
 
 object JsonHelpers {
+  val recordSeparatorChar = 31.toChar
+
   implicit val urlWrites: Writes[URL] = (o: URL) => JsString(o.toString)
   def toNDJson[T](serializable: T)(implicit tjs: Writes[T]) =
-    Json.toJson(serializable).toString() + 31.toChar
+    Json.toJson(serializable).toString() + recordSeparatorChar
 }
