d985e5224b7575ec781362e6b40b68efa666f76e
Rhys Mills <rhys.mills@guardian.co.uk>
2023-07-19 13:56:39 +0100

    Show diff modal in client

--- apps/rule-manager/client/src/ts/components/modals/Reason.tsx
+++ apps/rule-manager/client/src/ts/components/modals/Reason.tsx
@@ -1,8 +1,11 @@
 import {
+  EuiBadge,
   EuiButton,
   EuiButtonEmpty,
+  EuiCode,
   EuiFieldText,
   EuiFlexGroup,
+  EuiFlexItem,
   EuiForm,
   EuiFormRow,
   EuiModal,
@@ -12,69 +15,134 @@ import {
   EuiModalHeaderTitle,
   EuiSpacer,
   EuiText,
+  EuiTitle,
   useEuiTextDiff,
 } from "@elastic/eui";
 import { Label } from "../Label";
 import { FormEventHandler, useEffect, useState } from "react";
 import { RuleData } from "../hooks/useRule";
+import { useTags } from "../hooks/useTags";
+import { SectionHeader, Title } from "../RuleFormSection";
+import styled from "@emotion/styled";
 
 const modalFormId = "modal-form";
 
-type DiffedValues<T> = {
-  live: T;
-  draft: T;
+type DiffedFields = {
+  field: string;
+  live: DiffValue;
+  draft: DiffValue;
 }
 
-const findFieldsWithDiffs = (rule: RuleData): Record<string, DiffedValues<string | number | boolean>> => {
-  const draftEntries = Object.entries(rule.draft);
-  const liveEntries = Object.entries(rule.live.reduce((acc, cur) => cur.revisionId > acc.revisionId ? cur : acc))
-  const diffValues = draftEntries.reduce<Record<string, any>>((diffObject, draftEntry) => {
+type DiffValue = string | number | boolean | number[] | string[]
+
+const textDiffFields = ["description", "pattern", "replacement"];
+const comparisonDiffFields = ["ruleType", "category", "tags"];
+
+const findFieldsWithDiffs = (rule: RuleData): DiffedFields[] => {
+  const fieldsToProvideDiffFor = [...textDiffFields, ...comparisonDiffFields];
+  const draftEntries = Object.entries(rule.draft).filter(([key]) => fieldsToProvideDiffFor.includes(key));
+  const liveEntries = Object.entries(rule.live.reduce((acc, cur) => cur.revisionId > acc.revisionId ? cur : acc)).filter(([key]) => fieldsToProvideDiffFor.includes(key));
+
+  const diffValues = draftEntries.reduce((diffArray, draftEntry) => {
     const [draftKey, draftValue] = draftEntry;
     // Fields are all optional
-    const maybeEquivalentLiveEntry = liveEntries.find(([liveKey, liveValue]) => liveKey === draftKey)
+    const maybeEquivalentLiveEntry = liveEntries.find(([liveKey]) => liveKey === draftKey)
     if (maybeEquivalentLiveEntry){
-      const [liveKey, liveValue] = maybeEquivalentLiveEntry;
+      const [_, liveValue] = maybeEquivalentLiveEntry;
       const liveAndDraftValuesMatch = liveValue === draftValue;
       if (!liveAndDraftValuesMatch){
-        diffObject[draftKey] = {
+        diffArray.push({
+          field: draftKey,
           live: liveValue,
           draft: draftValue
-        }
+        })
       }
     }
-    return diffObject
-  }, {})
+    return diffArray
+  }, [] as DiffedFields[])
   return diffValues
   // Find rows that don't match and return only those rows that aren't identical in both
 };
 
-export const SingleTextDiff = ({draft, live, key}: {draft: string | number | boolean, live: string | number | boolean, key: string}) => {
+export const camelCaseToTitleCase = (str: string) => {
+  // Add space between strings
+  const result = str.replace(/([A-Z])/g,' $1');
+  // converting first character to uppercase and join it to the final string
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+export const TextDiff = ({draft, live, name}: {draft: DiffValue, live: DiffValue, name: string}) => {
   const [del, setDel] = useState(0);
   const [ins, setIns] = useState(0);
   const [rendered, textDiffObject] = useEuiTextDiff({ beforeText: live.toString(), afterText: draft.toString() })
-  useEffect(() => {
-    textDiffObject.forEach((el) => {
-      if (el[0] === 1) {
-        setIns((add) => add + 1);
-      } else if (el[0] === -1) {
-        setDel((sub) => sub + 1);
-      }
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return <EuiFlexGroup key={key}>
-    {rendered}
+  console.log({beforeText: live.toString(), afterText: draft.toString()})
+  return <EuiFlexGroup>
+      <EuiFlexItem>
+        <EuiFlexGroup>
+        <SectionHeader>
+            <Title>{camelCaseToTitleCase(name)}</Title>
+        </SectionHeader>
+        </EuiFlexGroup>
+        <EuiText>
+          <p>{rendered}</p>
+        </EuiText>
+      </EuiFlexItem>
+  </EuiFlexGroup>
+}
+
+const ComparisonPanel = styled.div`
+  background-color: #F1F4FA;
+  padding: 0.5rem;
+`
+
+export const Comparison = ({left, right} : {left: JSX.Element | JSX.Element[], right: JSX.Element | JSX.Element[]}) => {
+
+  return <EuiFlexGroup>
+    <EuiFlexItem grow><ComparisonPanel>{left}</ComparisonPanel></EuiFlexItem>
+    <EuiFlexItem grow><ComparisonPanel>{right}</ComparisonPanel></EuiFlexItem>
+  </EuiFlexGroup>
+}
+
+export const ComparisonDiff = ({draft, live, name}: {draft: DiffValue, live: DiffValue, name: string}) => {
+  // Transform tag ids to tag names
+  return <EuiFlexGroup>
+      <EuiFlexItem>
+        <EuiFlexGroup>
+        <SectionHeader>
+            <Title>{camelCaseToTitleCase(name)}</Title>
+        </SectionHeader>
+        </EuiFlexGroup>
+          <Comparison 
+            left={Array.isArray(draft) ? draft.map(item => <EuiBadge>{item}</EuiBadge>) : <EuiBadge>{draft}</EuiBadge>}
+            right={Array.isArray(live) ? live.map(item => <EuiBadge>{item}</EuiBadge>) : <EuiBadge>{live}</EuiBadge>}
+          />
+      </EuiFlexItem>
   </EuiFlexGroup>
 }
 
 export const ReasonDiff = ({rule}: {rule: RuleData | undefined}) => {
-  const diffedValues = rule ? findFieldsWithDiffs(rule) : null;
-  console.log(diffedValues);
+  const {tags} = useTags();
+  const diffedFields = rule ? findFieldsWithDiffs(rule) : null;
+  const textDiffs = diffedFields?.filter(diffedField => textDiffFields.includes(diffedField.field))
+  const comparisonDiffs = diffedFields?.filter(diffedField => comparisonDiffFields.includes(diffedField.field))
+  // if (diffedValues?.tags){
+  //   // Render tags as their names rather than their IDs
+  //   const diffedTags = diffedValues.tags;
+  //   diffedTags.live = (diffedTags.live as number[]).map(tagId => tags[tagId]?.name)
+  //   diffedTags.draft = (diffedTags.draft as number[]).map(tagId => tags[tagId]?.name)
+  //   //(diffedTags.draft as number[]).map(tagId => tags[tagId]);
+  //   console.log(tags)
+  // }
+
+  
   return <>
     <EuiSpacer />
     <EuiFlexGroup>
-      {diffedValues ? Object.entries(diffedValues).map(([key, diffedValue]) => <SingleTextDiff draft={diffedValue.draft} live={diffedValue.draft} key={key} />) : null}
-      <EuiText>What's changed:</EuiText>
+      <EuiFlexItem>
+        <EuiText>What's changed:</EuiText>
+        {textDiffs?.map(diffedField => <TextDiff draft={diffedField.draft} live={diffedField.live} name={diffedField.field} key={diffedField.field} />)}
+        {comparisonDiffs?.map(diffedField => <ComparisonDiff draft={diffedField.draft} live={diffedField.live} name={diffedField.field} key={diffedField.field} />)}
+      </EuiFlexItem>
     </EuiFlexGroup>
   </>
 }
@@ -119,7 +187,6 @@ export const ReasonModal = ({
         </EuiForm>
         <ReasonDiff rule={rule}/>
       </EuiModalBody>
-
       <EuiModalFooter>
         <EuiButtonEmpty onClick={onClose}>Cancel</EuiButtonEmpty>
         <EuiButton
