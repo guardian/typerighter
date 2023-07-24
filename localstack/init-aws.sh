d04f546ef07740ac972e8d3c374756a9c8859b44
Rhys Mills <rhys.mills@guardian.co.uk>
2023-07-24 14:27:44 +0100

    Pull the dictionary xml file from S3 on setup and replace setup-hooks with generic setup script

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -5,3 +5,4 @@ awslocal s3 mb s3://typerighter-app-local
 json='{"rules": []}'
 echo "$json" > typerighter-rules.json
 awslocal s3 cp typerighter-rules.json s3://typerighter-app-local/local/rules/typerighter-rules.json
+awslocal s3 cp ~/.gu/typerighter/typerighter-dictionary.xml s3://typerighter-app-local/local/dictionary/typerighter-dictionary.xml
