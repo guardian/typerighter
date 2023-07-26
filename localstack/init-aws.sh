8e06f65866f033f9220d5a6366f87336f872aedb
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-07-26 10:03:21 +0100

    Tweak paths in init-aws.sh

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -5,5 +5,5 @@ awslocal s3 mb s3://typerighter-app-local
 json='{"rules": []}'
 echo "$json" > typerighter-rules.json
 awslocal s3 cp typerighter-rules.json s3://typerighter-app-local/local/rules/typerighter-rules.json
-aws s3 cp --profile composer --region eu-west-1 s3://typerighter-app-dev/dictionary/typerighter-dictionary.xml ~/.gu/typerighter/typerighter-dictionary.xml
-awslocal s3 cp /etc/.gu/typerighter/typerighter-dictionary.xml s3://typerighter-app-local/local/dictionary/typerighter-dictionary.xml
+aws s3 cp --profile composer --region eu-west-1 s3://typerighter-app-dev/dictionary/typerighter-dictionary.xml ~/.gu/typerighter
+awslocal s3 cp ~/.gu/typerighter/typerighter-dictionary.xml s3://typerighter-app-local/local/dictionary/typerighter-dictionary.xml
