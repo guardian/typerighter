9bb1df3104134a7c0e09c4703db758403bb44ac0
Rhys Mills <rhys.mills@guardian.co.uk>
2023-07-28 09:32:04 +0100

    Rename dictionary location to /etc/gu and add comment

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -5,5 +5,5 @@ awslocal s3 mb s3://typerighter-app-local
 json='{"rules": []}'
 echo "$json" > typerighter-rules.json
 awslocal s3 cp typerighter-rules.json s3://typerighter-app-local/local/rules/typerighter-rules.json
-aws s3 cp --profile composer --region eu-west-1 s3://typerighter-app-dev/dictionary/typerighter-dictionary.xml ~/.gu/typerighter
-awslocal s3 cp ~/.gu/typerighter/typerighter-dictionary.xml s3://typerighter-app-local/local/dictionary/typerighter-dictionary.xml
+# Copying from the location in the localstack instance that Docker writes the dictionary file to
+awslocal s3 cp /etc/gu/typerighter/typerighter-dictionary.xml s3://typerighter-app-local/local/dictionary/typerighter-dictionary.xml
