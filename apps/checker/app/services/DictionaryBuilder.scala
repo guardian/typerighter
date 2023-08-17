0e7921ccd6793b03fb28faa9051230b2fa9594ae
Rhys Mills <rhys.mills@guardian.co.uk>
2023-08-17 13:52:16 +0100

    Add comments explaining origin to languagetool copies

--- apps/checker/app/services/DictionaryBuilder.scala
+++ apps/checker/app/services/DictionaryBuilder.scala
@@ -20,23 +20,8 @@ import morfologik.tools.DictCompile
 import morfologik.tools.FSACompile
 import morfologik.tools.SerializationFormat
 
-/* LanguageTool, a natural language style checker
- * Copyright (C) 2013 Daniel Naber (http://www.danielnaber.de)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA
+/* Scala conversion of Java original, derived from languagetool-tools: https://github.com/languagetool-org/languagetool/blob/master/languagetool-tools/src/main/java/org/languagetool/tools/DictionaryBuilder.java
+ * Hopefully we can publish from that repository and remove this version entirely.
  */
 
 /** Create a Morfologik binary dictionary from plain text data.
