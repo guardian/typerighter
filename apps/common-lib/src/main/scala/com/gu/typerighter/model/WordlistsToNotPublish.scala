5b2595bb20c762e88c4cdf07c1b5025e82a398dc
Rhys Mills <rhys.mills@guardian.co.uk>
2023-09-14 13:46:42 +0100

    Pull word to not publish list from s3 and use locally

--- apps/common-lib/src/main/scala/com/gu/typerighter/model/WordlistsToNotPublish.scala
+++ apps/common-lib/src/main/scala/com/gu/typerighter/model/WordlistsToNotPublish.scala
@@ -2,15 +2,20 @@ package com.gu.typerighter.model
 
 import play.api.libs.json.{Json, Reads, Writes}
 
-sealed trait TaggedWordlist {
-  val tag: String
-  val words: List[String]
+case class TaggedWordlist (
+   tag: String, words: List[String]
+)
+
+object TaggedWordlist {
+  implicit val writes: Writes[TaggedWordlist] = Json.writes[TaggedWordlist]
+  implicit val reads: Reads[TaggedWordlist] = Json.reads[TaggedWordlist]
 }
 
 case class WordTag(
     word: String,
     tag: String
 )
+
 case class WordlistsToNotPublish(rules: List[TaggedWordlist])
 
 object WordlistsToNotPublish {
