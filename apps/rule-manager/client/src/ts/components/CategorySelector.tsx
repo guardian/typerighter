f523b3b4ab4d45af534db89ad409925698ce2b1e
Samantha Gottlieb <samanthagottlieb31@gmail.com>
2023-10-10 19:57:34 +0100

    Prevent users from changing the Source for dictionary rules

--- apps/rule-manager/client/src/ts/components/CategorySelector.tsx
+++ apps/rule-manager/client/src/ts/components/CategorySelector.tsx
@@ -64,6 +64,7 @@ export const CategorySelector = ({
 				isClearable={true}
 				isCaseSensitive
 				fullWidth={true}
+				isDisabled={isDictionaryRule}
 				{...categoryErrors}
 			/>
 		</EuiFormRow>
