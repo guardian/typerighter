81e0432017cde76862cbaf947e060f19a59e9302
Rhys Mills <rhys.mills@guardian.co.uk>
2023-10-13 14:11:34 +0100

    Force Morfologik Speller dictionary cache to invalidate when there are new or edited dictionary rules
    Co-authored-by: Jonathon Herbert <jonathon.herbert@guardian.co.uk>

--- apps/checker/app/matchers/DictionaryMatcher.scala
+++ apps/checker/app/matchers/DictionaryMatcher.scala
@@ -1,6 +1,8 @@
 package matchers
 
 import com.gu.typerighter.model.{Category, DictionaryRule, RuleMatch}
+import com.google.common.cache.LoadingCache
+import morfologik.stemming.Dictionary
 import org.languagetool.{JLanguageTool, Language, ResultCache, UserConfig}
 import play.api.Logging
 import services.collins.{CollinsEnglish, MorfologikCollinsSpellerRule, SpellDictionaryBuilder}
@@ -19,10 +21,26 @@ class DictionaryMatcher(
   val cache: ResultCache = new ResultCache(10000)
   val userConfig: UserConfig = new UserConfig()
 
-  val instance = new JLanguageTool(language, cache, userConfig)
-
   // As a side effect, make sure the .dict artefact is available to languageTool
   new SpellDictionaryBuilder().buildDictionary(rules.map(rule => rule.word))
+
+  // This is an undesirable hack to force the MorfologikSpeller dictionary cache to invalidate. MorfologikSpeller
+  // is quite deep in a series of classes:
+  //   - MorfologikCollinsSpellerRule extends AbstractSpellerRule extends MorfologikSpellerRule which uses an
+  //     instance of MorfologikMultiSpeller which uses an instance of Morfologik Speller
+  // We don't want to re-implement all of those classes.
+  // Without invalidating the dictionary cache expires every 10 minutes, changes to dictionary rules will not be
+  // reflected in the DictionaryMatcher until a future re-compile of the dictionary when at least 10 minutes
+  // have expired. We want to invalidate the cache as soon as our DictionaryRules change.
+  // Ideally we would make changes to the LanguageTool library to allow this, but as a temporary hack, this allows
+  // our DictionaryMatcher to react to new or edited DictionaryRules.
+  val SpellerClass = Class.forName("org.languagetool.rules.spelling.morfologik.MorfologikSpeller")
+  val field = SpellerClass.getDeclaredField("dictCache")
+  field.setAccessible(true)
+  val dictionaryCache = field.get(field.getType()).asInstanceOf[LoadingCache[String, Dictionary]]
+  dictionaryCache.invalidateAll()
+
+  val instance = new JLanguageTool(language, cache, userConfig)
 
   // Disable default LanguageTool rules in the instance, i.e. anything that
   // isn't a MORFOLOGIK_RULE_COLLINS
