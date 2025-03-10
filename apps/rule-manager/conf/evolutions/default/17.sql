19cc3a52d40c9d8f0b47a71012ae7a34d50186b7
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2025-03-10 17:18:35 +0000

    Create indices concurrently

--- apps/rule-manager/conf/evolutions/default/17.sql
+++ apps/rule-manager/conf/evolutions/default/17.sql
@@ -1,18 +1,18 @@
 -- !Ups
 
-CREATE INDEX rules_draft_free_text_search_idx
+CREATE INDEX CONCURRENTLY rules_draft_free_text_search_idx
     ON public.rules_draft
         USING gist ((coalesce(rules_draft.pattern, '') ||
                     coalesce(rules_draft.description, '') ||
                     coalesce(rules_draft.replacement, '')) gist_trgm_ops);
 
 -- We order by updated_at by default - this index speeds up the initial call to display rules
-CREATE INDEX rules_draft_updated_at ON rules_draft(updated_at DESC);
+CREATE INDEX CONCURRENTLY rules_draft_updated_at ON rules_draft(updated_at DESC);
 
 -- `left` is used to index on a substring, as patterns can be very large, and there is a limit
 -- on the size of the data that can cause write errors (https://stackoverflow.com/a/70124913)
-CREATE INDEX rules_draft_pattern_idx_asc ON rules_draft(left(pattern, 20));
-CREATE INDEX rules_draft_pattern_idx_desc ON rules_draft(left(pattern, 20) DESC);
+CREATE INDEX CONCURRENTLY rules_draft_pattern_idx_asc ON rules_draft(left(pattern, 20));
+CREATE INDEX CONCURRENTLY rules_draft_pattern_idx_desc ON rules_draft(left(pattern, 20) DESC);
 
 -- !Downs
 
